4f2a0c165545e85c6c6fc0e9b2baccad1285506d
Mark Kuo <starryalley@gmail.com>
2019-05-22 21:28:01 +1000

    add sensor logger

---
+++ sensor_logger/google_sheet_api.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/user"
+	"path"
+	"strings"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/sheets/v4"
+)
+
+// copied from https://developers.google.com/sheets/api/quickstart/go
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	user, err := user.Current()
+	tokFile := ".google_sheet_token.json"
+	if err != nil {
+		log.Println(err)
+	} else {
+		tokFile = path.Join(user.HomeDir, tokFile)
+	}
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+// ref: https://gist.github.com/bati11/b2ba535f74c7bcb723a2ee46585814d8
+func insertRow(sheetService *sheets.Service, spreadsheetId string, sheetId, startId, endId int64) (*sheets.BatchUpdateSpreadsheetResponse, error) {
+	req := sheets.Request{
+		InsertDimension: &sheets.InsertDimensionRequest{
+			InheritFromBefore: false,
+			Range: &sheets.DimensionRange{
+				Dimension:  "ROWS",
+				StartIndex: startId,
+				EndIndex:   endId,
+				SheetId:    sheetId,
+			},
+		},
+	}
+	insertRowReq := sheets.BatchUpdateSpreadsheetRequest{
+		Requests: []*sheets.Request{&req},
+	}
+	return sheetService.Spreadsheets.BatchUpdate(spreadsheetId, &insertRowReq).Do()
+}
+
+func PrependRow(service *sheets.Service, spreadsheetId, rangeA1 string, row []interface{}) error {
+
+	a1 := strings.Split(rangeA1, "!")
+	if len(a1) != 2 {
+		return errors.New("unable to parse A1 notation " + rangeA1)
+	}
+	sheetId, err := getSpreadsheet(service, spreadsheetId, a1[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = insertRow(service, spreadsheetId, sheetId, 1, 2)
+	if err != nil {
+		log.Printf("sheet insert failed: %v", err)
+		return err
+	}
+
+	valueRange := &sheets.ValueRange{
+		MajorDimension: "ROWS",
+		Values: [][]interface{}{
+			row,
+		},
+	}
+	_, err = service.Spreadsheets.Values.Update(spreadsheetId, rangeA1, valueRange).ValueInputOption("USER_ENTERED").Do()
+	if err != nil {
+		log.Printf("sheet update failed: %v", err)
+		return err
+	}
+	return nil
+}
+
+func sheetId(s *sheets.Spreadsheet, sheetName string) (int64, error) {
+	for _, sheet := range s.Sheets {
+		if sheet.Properties.Title == sheetName {
+			return sheet.Properties.SheetId, nil
+		}
+	}
+	return 0, errors.New("couldn't find sheet:" + sheetName)
+}
+
+func InitGoogleSheet(credentialFilename string) (*sheets.Service, error) {
+	b, err := ioutil.ReadFile(credentialFilename)
+	if err != nil {
+		log.Fatalf("unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets")
+	if err != nil {
+		log.Printf("unable to parse client secret file to config: %v", err)
+		return nil, err
+	}
+	client := getClient(config)
+
+	srv, err := sheets.New(client)
+	if err != nil {
+		log.Printf("unable to retrieve Sheets client: %v", err)
+		return nil, err
+	}
+	return srv, nil
+}
+
+func getSpreadsheet(srv *sheets.Service, id string, sheetName string) (int64, error) {
+	spreadsheet, err := srv.Spreadsheets.Get(id).Do()
+	if err != nil {
+		log.Printf("unable to get sheet %v: %v", sheetName, err)
+		return 0, err
+	}
+
+	sheetId, err := sheetId(spreadsheet, sheetName)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+	return sheetId, nil
+}
---
+++ sensor_logger/main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"log"
+	"log/syslog"
+	"time"
+
+	"github.com/d2r2/go-dht"
+	logger "github.com/d2r2/go-logger"
+	"github.com/gofrs/flock"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/drivers/i2c"
+	"gobot.io/x/gobot/platforms/raspi"
+)
+
+// ========== Settings =========
+// update interval in minutes
+const UpdateInterval = 10
+
+// GPIO number for DHT temperature sensor
+const GPIOTemp = 4
+
+// Pin names for LED R,G,B pins
+const PinR = "11"
+const PinG = "13"
+const PinB = "15"
+
+// google sheet credential json file
+const GoogleSheetCredential = "/home/starryalley/.secret/google_sheet_credentials.json"
+
+// =============================
+
+func getTempHum(lock *flock.Flock) (float32, float32, error) {
+	logger.ChangePackageLogLevel("dht", logger.ErrorLevel)
+	var temperature, humidity float32
+	for {
+		locked, err := lock.TryLock()
+		if err != nil {
+			log.Printf("unable to lock for DHT22:%v\n", err)
+			return 0, 0, err
+		}
+		if locked {
+			defer lock.Unlock()
+			temperature, humidity, _, err =
+				dht.ReadDHTxxWithRetry(dht.DHT22, GPIOTemp, false, 10)
+			break
+		}
+	}
+	return temperature, humidity, nil
+}
+
+func main() {
+	// configure logger to write to syslog
+	logwriter, err := syslog.New(syslog.LOG_NOTICE, "SensorLogger")
+	if err == nil {
+		log.SetOutput(logwriter)
+		log.SetFlags(0)
+	}
+
+	// possible multi-process access to those hardware
+	fileLockLight := flock.New("/var/lock/tsl2561.lock")
+	fileLockTemp := flock.New("/var/lock/dht22.lock")
+
+	// initialise google sheet
+	service, err := InitGoogleSheet(GoogleSheetCredential)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// setup gobot
+	r := raspi.NewAdaptor()
+	led := gpio.NewRgbLedDriver(r, PinR, PinG, PinB)
+	lux := i2c.NewTSL2561Driver(r, i2c.WithBus(0), i2c.WithAddress(0x39), i2c.WithTSL2561Gain1X)
+
+	work := func() {
+		gobot.Every(UpdateInterval*time.Minute, func() {
+			now := time.Now()
+			temp, hum, err := getTempHum(fileLockTemp)
+			if err != nil {
+				log.Printf("read temperature failed:%v\n", err)
+				return
+			}
+			var broadband, ir uint16
+			for {
+				locked, err := fileLockLight.TryLock()
+				if err != nil {
+					log.Printf("unable to lock:%v\n", err)
+					return
+				}
+				if locked {
+					defer fileLockLight.Unlock()
+					// turn off LED to get accurate lux
+					led.Off()
+					defer led.On()
+					time.Sleep(500 * time.Millisecond)
+					broadband, ir, err = lux.GetLuminocity()
+					if err != nil {
+						log.Printf("read luminocity failed:%v\n", err)
+						return
+					}
+					break
+				}
+			}
+			light := lux.CalculateLux(broadband, ir)
+
+			log.Printf("T:%.01f°C H:%.01f%% BB:%v IR:%v Lux:%v\n",
+				temp, hum, broadband, ir, light)
+
+			// update to google sheet in a goroutine
+			go func() {
+				row := []interface{}{
+					now, //.Format("2006.01.02 15:04:05"),
+					temp,
+					hum,
+					broadband,
+					ir,
+					light,
+				}
+				err = PrependRow(service, "15Zyy0_swv2YazuL9UdZ4YYkPfaIwTpPNtPHLAlsLtcY", "RawData!A2:F2", row)
+				if err != nil {
+					log.Println(err)
+				}
+			}()
+
+		})
+	}
+
+	robot := gobot.NewRobot("SensorLoggerBot",
+		[]gobot.Connection{r},
+		[]gobot.Device{led, lux},
+		work,
+	)
+
+	robot.Start()
+}
